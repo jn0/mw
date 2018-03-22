1c2c8c9f95cac0149cd6b514963d95ee5052a3f5
jno <e.dvurechensky@ngslab.ru>
2018-03-22 12:26:18 +0300

    improve wait routines

--- watch.sh
+++ watch.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
-INTERVAL=60
+: ${INTERVAL:=60}
+
+temp=$(mktemp --tmpdir=/var/tmp --suffix=.watch)
+trap 'rm -f "$temp"' EXIT
 
 fetch() {
     local url=${1:-wttr.in}
     shift
-    curl -H "Accept-Language: ru" -Ss "${url}" "$@"
+    curl -o "$temp" -H "Accept-Language: ru" -Ss "${url}" "$@"
+    cat "$temp"
 }
 
-# adjust to :00 secs
-while :; do
-    TTW=$(( `date '+1%S'` - 100 ))
-    echo -ne "${TTW} ...\\r"
-    [ ${TTW} -eq 0 ] && break
-    sleep 0.5
-done
+filter() {
+    grep -v "Попробуйте" |
+    grep -v "новые фичи"
+}
+
+adjust() { # to interval
+    local -i TTW=0
+
+    while :; do
+        TTW=$(( (`date '+1%S'` - 100) % INTERVAL ))
+        echo -ne "${TTW}/${INTERVAL} ...\\r"
+        [ ${TTW} -eq 0 ] && break
+        sleep 0.5
+    done
+}
 
 command=${1:-fetch} ; shift
 
 while clear; do
     date '+%F %T %z'
-    "$command" "$@"
-    sleep ${INTERVAL:-5}
-done | grep -v "Попробуйте" | grep -v "новые фичи"
+    "$command" "$@" | filter
+    sleep 0.5
+    adjust
+done
 
 # EOF #
